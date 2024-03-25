c65a51eeadc2aa72373046369ec2967a8eab0ae5
eric-j-owen <145154417+eric-j-owen@users.noreply.github.com>
2024-03-25 15:55:26 -0400

    adds basic form input to fetch data functionality

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-// a96b9673cecd4f1282b131742241903
-// http://api.weatherapi.com/v1
-
-//enter location
-//return weather data for location
-
 async function fetchWeather(inputZip) {
   const zip = inputZip;
-  const res = await fetch(`https://api.weatherapi.com/v1/current.json?key=a96b9673cecd4f1282b131742241903&q=${zip}`)
+  const res = await fetch(
+    `https://api.weatherapi.com/v1/current.json?key=a96b9673cecd4f1282b131742241903&q=${zip}`
+  );
   const data = await res.json();
   return data;
 }
 
-fetchWeather(30542);
+document.querySelector("button").addEventListener("click", async (e) => {
+  e.preventDefault();
+  const data = await fetchWeather(document.querySelector("input").value);
+  console.log(data)
+  document.querySelector(".location").textContent = await `${data.location.name}, ${data.location.region}`;
+  document.querySelector(".temp").textContent = await `${data.current.condition.text} at ${data.current.temp_f} degrees`
+});
